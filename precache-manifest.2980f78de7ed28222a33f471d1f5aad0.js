208078cb661d4384d5ecdecd20dcba1b1ebf7b23
developer-jfr <85294383+developer-jfr@users.noreply.github.com>
2021-06-11 20:03:54 +0600

    Add files via upload

---
+++ precache-manifest.2980f78de7ed28222a33f471d1f5aad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f9a4538c7da1e921472fdb01accc21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "154e18a90607b8e72ec5",
+    "url": "./static/css/2.240f97b7.chunk.css"
+  },
+  {
+    "revision": "1d9490ce6d03663ec3e4",
+    "url": "./static/css/main.d9640d40.chunk.css"
+  },
+  {
+    "revision": "154e18a90607b8e72ec5",
+    "url": "./static/js/2.246e2884.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.246e2884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9490ce6d03663ec3e4",
+    "url": "./static/js/main.e908c68d.chunk.js"
+  },
+  {
+    "revision": "afd9264ea911281a308c",
+    "url": "./static/js/runtime-main.031aed12.js"
+  }
+]);
